d72c66a8d5d59fe79c7edf7562b398bf7179c28b
Ilya Petrash <gil9red@gmail.com>
2014-11-11 03:40:22 +0600

    Обновлен класс src_lang.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- src_lang.py
+++ src_lang.py
@@ -1,11 +1,33 @@
 __author__ = 'ipetrash'
 
 
+""""""
+
+
 class Src_Lang:
     """"""
 
+    # Описание
+    # Язык оригинала (для переводов).
+    #
+    # Подчиненные элементы
+    # Нет дочерних элементов, содержит текст - двухбуквенный код языка.
+    #
+    # Подчинен
+    # Может содержаться в следующих элементах:
+    # <title-info> - 0..1 (один, опционально);
+    # <src-title-info>- 0..1 (один, опционально).
+    #
+    # Пример использования
+    # <src-lang>en</src-lang>
+
     def __init__(self):
-        pass
+        self.value = None
+
+    # TODO: Доделать
 
     def get_source(self):
-        pass
+        if not self.value:
+            raise NameError('Не указан язык (self.value is None).')
+
+        return '<src-lang>{}</src-lang>'.format(self.value)
