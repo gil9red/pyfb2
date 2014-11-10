ac5984262d2c2f2f13511a4a5c828c15e7fde635
Ilya Petrash <gil9red@gmail.com>
2014-11-11 02:08:55 +0600

    Добавлен модуль keywords.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ keywords.py
@@ -0,0 +1,32 @@
+__author__ = 'ipetrash'
+
+
+""""""
+
+
+class Keywords:
+    """"""
+
+    # Описание
+    # Список ключевых слов к книге. Предназначен для использования
+    # поисковыми системами.
+    #
+    # Подчиненные элементы
+    # Нет подчиненных элементов.
+    # Содержит текстовую строку ? собственно список ключевых слов.
+    #
+    # Подчинен
+    # <title-info> - 0..1 (один, опционально);
+    # <src-title-info> - 0..1 (один, опционально)
+
+    def __init__(self):
+        self.words = []
+
+    def add_keywords(self, word):
+        self.words.append(word)
+
+    def remove_keywords(self, word):
+        self.words.remove(word)
+
+    def get_source(self):
+        return '<keywords>{}</keywords>'.format(', '.join(self.words))
