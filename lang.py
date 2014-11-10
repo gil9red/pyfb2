86a9df6cc480c03f49d6c3650aee2be67726c7fd
Ilya Petrash <gil9red@gmail.com>
2014-11-11 01:42:49 +0600

    Обновлен класс lang.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- lang.py
+++ lang.py
@@ -4,8 +4,28 @@ __author__ = 'ipetrash'
 class Lang:
     """"""
 
+    # Описание
+    # Язык книги (произведения), если содержится в <title-info>; либо язык
+    # оригинала (для переводов), если в <src-title-info>.
+    #
+    # Подчиненные элементы
+    # Нет дочерних элементов, содержит текст - двухбуквенный код языка.
+    #
+    # Подчинен
+    # Может содержаться в следующих элементах:
+    # <title-info> 1 (один, обязателен);
+    # <src-title-info> 1 (один, обязателен) с версии 2.1.
+    #
+    # Пример использования
+    # <lang>ru</lang>
+
     def __init__(self):
-        pass
+        self.value = None
+
+    # TODO: Доделать
 
     def get_source(self):
-        pass
+        if not self.value:
+            raise NameError('Не указан язык (self.value is None).')
+
+        return '<lang>{}</lang>'.format(self.value)
