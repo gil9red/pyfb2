d856de122522a4f9e11fe493c0e414e904955c72
Ilya Petrash <gil9red@gmail.com>
2014-11-11 01:34:07 +0600

    Обновлен класс title_info.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- title_info.py
+++ title_info.py
@@ -1,11 +1,7 @@
 __author__ = 'ipetrash'
 
 
-from genre import Genre
-from author import Author
 from book_title import Book_Title
-from lang import Lang
-from src_lang import Src_Lang
 
 
 class Title_Info:
@@ -18,15 +14,15 @@ class Title_Info:
     # <description> - 1 (один, обязателен)
 
     def __init__(self):
-        self.genre = [Genre()]  # <genre> - 1..n (любое число, один обязaтелен);
-        self.author = [Author()]  # <author> - 1..n (любое число, один обязaтелен);
-        self.book_title = [Book_Title()]  # <book-title> - 1 (один, обязателен);
+        self.genre = []  # <genre> - 1..n (любое число, один обязaтелен);
+        self.author = []  # <author> - 1..n (любое число, один обязaтелен);
+        self.book_title = Book_Title()  # <book-title> - 1 (один, обязателен);
         self.annotation = None  # <annotation> - 0..1 (один, опционально);
         self.keywords = None  # <keywords> - 0..1 (один, опционально);
         self.date = None  # <date> - 0..1 (один, опционально);
         self.coverpage = None  # <coverpage> - 0..1 (один, опционально);
-        self.lang = Lang()  # <lang> - 1 (один, обязателен);
-        self.src_lang = Src_Lang()  # <src-lang> - 0..1 (один, опционально);
+        self.lang = None  # <lang> - 1 (один, обязателен);
+        self.src_lang = None  # <src-lang> - 0..1 (один, опционально);
         self.translator = []  # <translator> - 0..n (любое число, опционально);
         self.sequence = []  # <sequence> - 0..n (любое число, опционально).
 
