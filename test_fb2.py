9bf763c213b08d1b4d1aa58f48612cb75eb1ffb3
Ilya Petrash <gil9red@gmail.com>
2014-11-11 03:59:08 +0600

    Обновлено.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- test_fb2.py
+++ test_fb2.py
@@ -12,20 +12,22 @@ if __name__ == '__main__':
     title_info.book_title.text = "Мое произведение"
     title_info.book_title.lang = "ru"
     title_info.lang.value = "ru"
+    title_info.src_lang.value = "en"
     title_info.keywords.add_keywords("книга")
     title_info.keywords.add_keywords("foo")
     title_info.keywords.add_keywords("github")
+    title_info.date.set_date(16, 11, 2014)
     
-    # book.description.title_info.genre = []  # <genre> - 1..n (любое число, один обязaтелен);
-    # book.description.title_info.author = []  # <author> - 1..n (любое число, один обязaтелен);
-    # book.description.title_info.book_title = Book_Title()  # <book-title> - 1 (один, обязателен);
-    # book.description.title_info.annotation = None  # <annotation> - 0..1 (один, опционально);
-    # book.description.title_info.keywords = None  # <keywords> - 0..1 (один, опционально);
-    # book.description.title_info.date = None  # <date> - 0..1 (один, опционально);
-    # book.description.title_info.coverpage = None  # <coverpage> - 0..1 (один, опционально);
-    # book.description.title_info.lang = Lang()  # <lang> - 1 (один, обязателен);
-    # book.description.title_info.src_lang = None  # <src-lang> - 0..1 (один, опционально);
-    # book.description.title_info.translator = []  # <translator> - 0..n (любое число, опционально);
-    # book.description.title_info.sequence = []  # <sequence> - 0..n (любое число, опционально).
+    # title_info.genre = []  # <genre> - 1..n (любое число, один обязaтелен);
+    # title_info.author = []  # <author> - 1..n (любое число, один обязaтелен);
+    # + title_info.book_title = Book_Title()  # <book-title> - 1 (один, обязателен);
+    # title_info.annotation = None  # <annotation> - 0..1 (один, опционально);
+    # + title_info.keywords = None  # <keywords> - 0..1 (один, опционально);
+    # + title_info.date = None  # <date> - 0..1 (один, опционально);
+    # title_info.coverpage = None  # <coverpage> - 0..1 (один, опционально);
+    # + title_info.lang = Lang()  # <lang> - 1 (один, обязателен);
+    # + title_info.src_lang = None  # <src-lang> - 0..1 (один, опционально);
+    # title_info.translator = []  # <translator> - 0..n (любое число, опционально);
+    # title_info.sequence = []  # <sequence> - 0..n (любое число, опционально).
 
     print(book.get_source())
