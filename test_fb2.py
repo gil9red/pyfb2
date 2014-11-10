0c53d4045bba6b88deb5ad5b60ebf04c8a1caef3
Ilya Petrash <gil9red@gmail.com>
2014-11-11 02:11:09 +0600

    Обновлено.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- test_fb2.py
+++ test_fb2.py
@@ -3,10 +3,29 @@ import fb2
 __author__ = 'ipetrash'
 
 # TODO: xml:lang = http://htmlbook.ru/html/value/lang
+# http://www.fictionbook.org/index.php/Пример_документа_в_FB2
 
 if __name__ == '__main__':
     book = fb2.FB2()
-    book.description.title_info.book_title.text = "Мое произведение"
-    book.description.title_info.book_title.lang = "ru"
-    book.description.title_info.lang.value = "ru"
+
+    title_info = book.description.title_info
+    title_info.book_title.text = "Мое произведение"
+    title_info.book_title.lang = "ru"
+    title_info.lang.value = "ru"
+    title_info.keywords.add_keywords("книга")
+    title_info.keywords.add_keywords("foo")
+    title_info.keywords.add_keywords("github")
+    
+    # book.description.title_info.genre = []  # <genre> - 1..n (любое число, один обязaтелен);
+    # book.description.title_info.author = []  # <author> - 1..n (любое число, один обязaтелен);
+    # book.description.title_info.book_title = Book_Title()  # <book-title> - 1 (один, обязателен);
+    # book.description.title_info.annotation = None  # <annotation> - 0..1 (один, опционально);
+    # book.description.title_info.keywords = None  # <keywords> - 0..1 (один, опционально);
+    # book.description.title_info.date = None  # <date> - 0..1 (один, опционально);
+    # book.description.title_info.coverpage = None  # <coverpage> - 0..1 (один, опционально);
+    # book.description.title_info.lang = Lang()  # <lang> - 1 (один, обязателен);
+    # book.description.title_info.src_lang = None  # <src-lang> - 0..1 (один, опционально);
+    # book.description.title_info.translator = []  # <translator> - 0..n (любое число, опционально);
+    # book.description.title_info.sequence = []  # <sequence> - 0..n (любое число, опционально).
+
     print(book.get_source())
