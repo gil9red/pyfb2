66c8b0d829a0ab3e7f916993f37b9295aabd0059
Ilya Petrash <gil9red@gmail.com>
2014-11-12 00:49:25 +0600

    Добавлена поддержка тэга stylesheet.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- fb2.py
+++ fb2.py
@@ -4,6 +4,7 @@ __author__ = 'ipetrash'
 """Модуль для создания документов FictionBook версии 2.0 (fb2)."""
 
 
+from stylesheet import Stylesheet
 from description import Description
 from body import Body
 from binary import Binary
@@ -12,6 +13,16 @@ from binary import Binary
 class FB2:
     """"""
 
+    # Описание
+    # Корневой элемент документа.
+    #
+    # Подчиненные элементы
+    # Должен содержать в перечисленном порядке:
+    # <stylesheet> - 0..n (любое число, опционально);
+    # <description> - 1 (один, обязателен);
+    # <body> - 1..n (любое число, один обязaтелен);
+    # <binary> - 0..n (любое число, опционально).
+
     # Раздел FictionBook состоит из вложенных подразделов в указанном ниже порядке:
     # <description> - который описывает заголовок документа. Одно и только одно вхождение.
     # (фразы вроде "одно и только одно вхождение" говорят, сколько раз подряд может идти
@@ -19,10 +30,12 @@ class FB2:
     # <body> - описывает тело документа. Одно или более вхождений.
     # <binary> - содержит приложенные к файлу двоичные объекты - картинки и прочее.
     # Любое число вхождений.
+    #
     # Иными словами, присутствуют как минимум разделы <description> с <body>, а
     # остальное - по необходимости.
 
     def __init__(self):
+        self.stylesheet = Stylesheet()  # Любое число, опционально
         self.description = Description()  # Одно и только одно вхождение
         self.body = [Body()]  # Одно или более вхождений
         self.binary = []  # Любое число вхождений
@@ -37,6 +50,7 @@ class FB2:
         source_fb2 = ''
         source_fb2 += ('<FictionBook xmlns="http://www.gribuser.ru/xml/fictionbook/2.0" '
                        'xmlns:l="http://www.w3.org/1999/xlink">')
+        source_fb2 += self.stylesheet.get_source()
         source_fb2 += self.description.get_source()
         for b in self.body:
             source_fb2 += b.get_source()
