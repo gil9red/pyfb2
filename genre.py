eae8f708189774e0ae8a8a9749a7102b89da1fe0
Ilya Petrash <gil9red@gmail.com>
2014-11-11 17:12:55 +0600

    Обновлен класс Genre. Добавлен класс GenreItem.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- genre.py
+++ genre.py
@@ -1,7 +1,12 @@
+from lxml.objectify import NoneElement
+
 __author__ = 'ipetrash'
 
 
-class Genre:
+""""""
+
+
+class GenreItem:
     """Описывает жанровую принадлежность книги. Используется для помещения
     книги в рубрикатор библиотеки, по этой причине список возможных жанров
     жестко задается. Допускается указание нескольких жанров."""
@@ -11,22 +16,45 @@ class Genre:
     # задающее субъективное процентное соответствие данному жанру.
     #
     # Содержит текст - обозначение жанра из списка жанров.
-    # http://www.fictionbook.org/index.php/%D0%96%D0%B0%D0%BD%D1%80%D1%8B_FictionBook_2.1
+    # http://www.fictionbook.org/index.php/Жанры_FictionBook_2.1
 
     # Пример:
     # Вестерн с элементами детектива можно описать следующим образом:
     # <genre>adv_western</genre>
     # <genre match="20">detective</genre>
 
-    def __init__(self):
-        self.text = None
-        self.match = None
+    def __init__(self, name=None, match=None):
+        self.name = name
+        self.match = match
 
     def get_source(self):
-        if not self.text:
-            raise NameError('Не определен жанр (self.text is None).')
+        if not self.name:
+            raise NameError('Не определен жанр.')
 
         if self.match:
-            return '<genre match="{}">{}</genre>'.format(self.match, self.text)
+            return '<genre match="{}">{}</genre>'.format(self.match, self.name)
         else:
-            return '<genre>{}</genre>'.format(self.text)
+            return '<genre>{}</genre>'.format(self.name)
+
+
+class Genre:
+    """"""
+
+    # TODO: доделать
+
+    def __init__(self):
+        self.list = []
+
+    def append(self, name, match=None):
+        self.list.append(GenreItem(name, match))
+
+    def get_source(self):
+        # Список жанров должен иметь как минимум один жанр
+        if not self.list:
+            raise NameError('Список жанров пуст.')
+
+        source = ''
+        for g in self.list:
+            source += g.get_source()
+
+        return source
