c2c469ee4dbbe8ad30c5069b75b917fbd78a729e
Ilya Petrash <gil9red@gmail.com>
2014-11-12 00:48:49 +0600

    Добавлен модуль stylesheet.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ stylesheet.py
@@ -0,0 +1,76 @@
+__author__ = 'ipetrash'
+
+
+""""""
+
+
+class StylesheetItem:
+    """"""
+
+    # Описание
+    # Здесь содержится таблица стилей, предназначенная для использования
+    # программами отображения (или преобразования) книги.
+    # На каждый тип таблицы (например ?text/css?) задается отдельный
+    # экземпляр <stylesheet>
+    #
+    # Атрибуты
+    # type - Тип таблицы стилей, например "text/css".
+    #
+    # Подчиненные элементы
+    # Нет подчиненных элементов.
+    # Содержит текст, который трактуется программой согласно типу (аттрибут "type")
+    # и реализации.
+    #
+    # Пример использования
+    # Пример 1.
+    # <stylesheet type="text/css">
+    #     body{
+    #         text-align:justify
+    #     }
+    #     div.stanza{
+    #         margin: 0.4em 0em 0.4em 1em;
+    #     }
+    # </stylesheet>
+    #
+    # Пример 2.
+    # <stylesheet type="text/css">
+    #     span.semi-condensed{
+    #        'font-stretch : semi-condensed;
+    #     }
+    # </stylesheet>
+    #
+    # ...
+    #
+    # <p>Normal text <style name="semi-condensed">Semi-condensed text</style> Normal text</p>
+    #
+    # ...
+    #
+
+    def __init__(self, type_stylesheet=None, text_stylesheet=None):
+        self.type = type_stylesheet
+        self.text = text_stylesheet
+
+    def get_source(self):
+        if not self.type or not self.text:
+            raise NameError('Не описан тип стиля или содержимое стиля.')
+
+        return '<stylesheet type="{}">{}</stylesheet>'.format(self.type, self.text)
+
+
+class Stylesheet:
+    """"""
+
+    # TODO: доделать
+
+    def __init__(self):
+        self.list = []
+
+    def append(self, type_stylesheet, text_stylesheet):
+        self.list.append(StylesheetItem(type_stylesheet, text_stylesheet))
+
+    def get_source(self):
+        source = ''
+        for s in self.list:
+            source += s.get_source()
+
+        return source
