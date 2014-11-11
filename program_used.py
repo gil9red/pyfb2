59de5b24cdd90f49492d300831defb09bf0045f2
Ilya Petrash <gil9red@gmail.com>
2014-11-12 05:32:56 +0600

    Добавлен модуль program_used.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ program_used.py
@@ -0,0 +1,50 @@
+__author__ = 'ipetrash'
+
+
+""""""
+
+
+class Program_Used:
+    """"""
+
+    # Описание
+    # Перечисляются программы, которые использовались при подготовке документа.
+    #
+    # Атрибуты
+    # xml:lang (опционально) - язык.
+    #
+    # Подчиненные элементы
+    # Нет дочерних элементов, содержит текст.
+    #
+    # Подчинен
+    # Может сожержаться в следующих элементах:
+    # <document-info> (опционально).
+    #
+    # Пример использования
+    # <program-used>Dn/2, Opera 8.50, Bred3</program-used>
+
+    # TODO: доделать
+
+    def __init__(self):
+        self.lang = None
+        self.list = []
+
+    def append(self, program):
+        if isinstance(program, list):
+            for p in program:
+                self.append(p)
+        else:
+            if not program in self.list:
+                self.list.append(program)
+
+    def get_source(self):
+        if not self.list:
+            raise NameError('Список использованных программ пуст.')
+
+        source = '<program-used'
+        if self.lang:
+            source += ' xml:lang="{}"'.format(self.lang)
+        source += '>'
+        source += ', '.join(self.list)
+        source += '</program-used>'
+        return source
