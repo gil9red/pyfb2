ad9f5eef766fcfa1164976afd5e4a018710b5908
Ilya Petrash <gil9red@gmail.com>
2014-11-12 01:57:52 +0600

    Обновлен класс coverpage.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- coverpage.py
+++ coverpage.py
@@ -31,8 +31,8 @@ class Coverpage:
         self.images.append(image)
 
     def get_source(self):
-        if not self.images:
-            raise NameError('Список изображений пуст.')
+        # if not self.images:
+        #     raise NameError('Список изображений пуст.')
 
         source = '<coverpage>'
         for im in self.images:
