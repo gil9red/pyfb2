cd296df14457da0f26b171f1dc5a99ca1cf4c3fe
Ilya Petrash <gil9red@gmail.com>
2014-11-13 02:47:40 +0600

    Обновлен модуль epigraph.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- epigraph.py
+++ epigraph.py
@@ -1,11 +1,49 @@
 __author__ = 'ipetrash'
 
 
+""""""
+
+
 class Epigraph:
     """"""
 
+    # Описание
+    # Эпиграф
+    #
+    # Атрибуты
+    # id (опциональный) - Идентификатор (якорь, метка) для ссылок на данный элемент
+    #
+    # Подчиненные элементы
+    # Содержит в перечисленном порядке элементы:
+    # Произвольный набор (в произвольном количестве)
+    # <p>;
+    # <poem>;
+    # <cite>;
+    # <empty-line>;
+    # <text-author> 0..n (любое число, опционально).
+    #
+    # Подчинен
+    # Может содержаться в следующих элементах:
+    # <body>;
+    # <section>;
+    # <poem>.
+    #
+    # Пример использования
+    # <epigraph>
+    # <p>"The truth is the one thing that nobody will believe."</p>
+    # <text-author>GEORGE BERNARD SHAW 1856-1950</text-author>
+    # </epigraph>
+
+    # TODO: доделать
+
     def __init__(self):
-        pass
+        self.id = None
+        # TODO: добавить подчиненные элементы
 
     def get_source(self):
-        pass
+        source = '<epigraph'
+        if self.id:
+            source += ' id="{}"'.format(self.id)
+        source += '>'
+        source += '</epigraph>'
+        return source
