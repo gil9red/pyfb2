97e5d60b8eafc2ec7b55fa4fa363962eb31b149d
Ilya Petrash <gil9red@gmail.com>
2014-11-12 22:17:13 +0600

    Обновлен модуль description.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- description.py
+++ description.py
@@ -3,6 +3,7 @@ __author__ = 'ipetrash'
 
 from title_info import Title_Info
 from document_info import Document_Info
+from publish_info import Publish_Info
 
 
 class Description:
@@ -33,15 +34,21 @@ class Description:
     def __init__(self):
         self.title_info = Title_Info()  # 1 (один, обязателен);
         self.document_info = Document_Info()  # 1 (один, обязателен);
-        self.publish_info = None  # 0..1 (один, опционально);
+        self.__publish_info = None  # 0..1 (один, опционально);
         self.custom_info = []  # 0..n (любое число, опционально);
+
+    def get_publish_info(self):
+        if not self.__publish_info:
+            self.__publish_info = Publish_Info()
+        return self.__publish_info
+    publish_info = property(get_publish_info)
 
     def get_source(self):
         source = '<description>'
         source += self.title_info.get_source()
         source += self.document_info.get_source()
-        if self.publish_info:
-            source += self.publish_info.get_source()
+        if self.__publish_info:
+            source += self.__publish_info.get_source()
         for ci in self.custom_info:
             source += ci.get_source()
         source += '</description>'
