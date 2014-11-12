53a1c26e557b7fd4d6720fc61aea5cfcd294d36e
Ilya Petrash <gil9red@gmail.com>
2014-11-12 06:28:24 +0600

    Обновлен модуль document_info.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- document_info.py
+++ document_info.py
@@ -1,6 +1,16 @@
 __author__ = 'ipetrash'
 
 
+""""""
+
+
+from author import Author
+from date import Date
+from program_used import Program_Used
+from src_url import Src_Url
+from src_ocr import Src_Ocr
+
+
 class Document_Info:
     """"""
 
@@ -18,12 +28,49 @@ class Document_Info:
     # <history> - 0..1 (один, опционально);
     # <publisher> - 0..n (любое число, опционально) с версии 2.2.
 
+    # TODO: доделать
 
     def __init__(self):
-        pass
+        self.author = Author()  # 1..n (любое число, один обязaтелен);
+        self.__program_used = None  # 0..1 (один, опционально);
+        self.date = Date()  # 1 (один, обязателен);
+        self.src_url = Src_Url()  # 0..n (любое число, опционально);
+        self.__src_ocr = None  # 0..1 (один, опционально);
+        self.id = None  # 1 (один, обязателен);
+        self.version = None  # 1 (один, обязателен);
+        self.__historyhistory = None  # 0..1 (один, опционально);
+        self.publisher = None  # 0..n (любое число, опционально) с версии 2.2.
+
+    def get_program_used(self):
+        if not self.__program_used:
+            self.__program_used = Program_Used()
+        return self.__program_used
+    program_used = property(get_program_used)
+
+    def get_src_ocr(self):
+        if not self.__src_ocr:
+            self.__src_ocr = Src_Ocr()
+        return self.__src_ocr
+    src_ocr = property(get_src_ocr)
 
     def get_source(self):
-        # TODO: доделать
         source = '<document-info>'
+        source += self.author.get_source()
+        if self.__program_used:
+            source += self.__program_used.get_source()
+
+        source += self.date.get_source()
+        source += self.src_url.get_source()
+
+        if self.__src_ocr:
+            source += self.__src_ocr.get_source()
+
+        # source += self.id.get_source()
+        # source += self.version.get_source()
+        #
+        # if self.__history:
+        #     source += self.__history.get_source()
+        #
+        # source += self.publisher.get_source()
         source += '</document-info>'
         return source
