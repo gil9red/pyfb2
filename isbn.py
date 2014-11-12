2085a313a2eed76ff208c1435efe90129e834947
Ilya Petrash <gil9red@gmail.com>
2014-11-12 22:48:49 +0600

    Добавлен модуль isbn.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ isbn.py
@@ -0,0 +1,48 @@
+__author__ = 'ipetrash'
+
+
+""""""
+
+
+class Isbn:
+    """"""
+
+    # Описание
+    # ISBN оригинальной (бумажной) книги.
+    #
+    # Атрибуты
+    # xml:lang (опционально) - язык.
+    #
+    # Подчиненные элементы
+    # Нет подчиненных элементов, содержит текстовую строку ? собственно ISBN книги.
+    #
+    # Подчинен
+    # Может содержаться в следующих элементах:
+    # <publish-info> (опционально).
+    #
+    # Пример использования
+    # <publish-info>
+    #   <book-name>Долгин А.Б. Экономика символического обмена</book-name>
+    #   <publisher>Инфра-М</publisher>
+    #   <city>Москва</city>
+    #   <year>2006</year>
+    #   <isbn>5-16-002911-7</isbn>
+    # </publish-info>
+
+    # TODO: доделать
+
+    def __init__(self):
+        self.lang = None
+        self.text = None
+
+    def get_source(self):
+        if not self.text:
+            raise NameError('Не указано ISBN оригинальной книги.')
+
+        source = '<isbn'
+        if self.lang:
+            source += ' xml:lang="{}"'.format(self.lang)
+        source += '>'
+        source += self.text
+        source += '</isbn>'
+        return source
