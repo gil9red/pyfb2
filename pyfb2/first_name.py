7be1104b872c5c163ec77cc77c4d9982e950489d
Ilya Petrash <gil9red@gmail.com>
2014-11-15 03:16:01 +0600

    Добавлен модуль first_name.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ pyfb2/first_name.py
@@ -0,0 +1,48 @@
+__author__ = 'ipetrash'
+
+
+""""""
+
+
+class First_Name:
+    """"""
+
+    # Описание
+    # Имя автора, переводчика или правообладателя.
+    #
+    # Атрибуты
+    # xml:lang (опциональный) - язык.
+    #
+    # Подчиненные элементы
+    # Нет дочерних элементов, содержит текст - собственно имя.
+    #
+    # Подчинен
+    # Может содержаться в следующих элементах:
+    # <author>;
+    # <translator>;
+    # <publisher> с версии 2.2.
+    #
+    # Пример использования
+    # <author>
+    #   <first-name>Robert</first-name>
+    #   <middle-name>Anson</middle-name>
+    #   <last-name>Heinlein</last-name>
+    # </author>
+
+    # TODO: доделать
+
+    def __init__(self):
+        self.lang = None
+        self.text = None
+
+    def get_source(self):
+        if not self.text:
+            raise NameError('Не указано имя автора, переводчика или правообладателя.')
+
+        source = '<first-name'
+        if self.lang:
+            source += ' xml:lang="{}"'.format(self.lang)
+        source += '>'
+        source += self.text
+        source += '</first-name>'
+        return source
