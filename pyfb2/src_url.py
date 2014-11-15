59011d723aca76a18b8c5bdfaf96d34dd36c418f
Ilya Petrash <gil9red@gmail.com>
2014-11-16 02:31:54 +0600

    Обновлен модуль title.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- pyfb2/src_url.py
+++ pyfb2/src_url.py
@@ -24,7 +24,7 @@ class Src_Url:
     # <src-url>http://4pda.ru/mag.php</src-url>
 
     def __init__(self):
-        self.list = []
+        self.__list = []
 
     def append(self, url):
         # TODO: дубликат кода есть в program_used
@@ -33,15 +33,15 @@ class Src_Url:
                 self.append(p)
         else:
             # Добавим url, если ее нет в списке:
-            if not url in self.list and url:
-                self.list.append(url)
+            if not url in self.__list and url:
+                self.__list.append(url)
             else:
                 print('Элемент "{}" уже есть в списке.'.format(url))
 
     def get_source(self):
         source = ''
 
-        for u in self.list:
+        for u in self.__list:
             source += '<src-url>{}</src-url>'.format(u)
 
         return source
